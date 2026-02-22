fbd6f188cf905da5feb1d307a951657b47ced058
Giants vs Packers <LeopragAG@gmail.com>
2026-02-22 07:30:44 -0700

    Update server.js

--- server.js
+++ server.js
@@ -84,8 +84,7 @@ app.post('/burn-captions', async (req, res) => {
       'Shadow=1','Alignment=2','MarginV=30'
     ].join(',');
     const safeSub = sub.replace(/'/g, "\\'");
-    const cmd = `${ffmpegPath} -y -i "${inp}" -vf "scale=1080:1920:force_original_aspect_ratio=decrease,pad=1080:1920:(ow-iw)/2:(oh-ih)/2,subtitles='${safeSub}':force_style='${style}'" -c:v libx264 -preset ultrafast -crf 23 -profile:v high -level 4.1 -c:a aac -b:a 128k -movflags +faststart "${out}"`;
-    console.log(`▶ [${videoName}] Burning captions...`);
+    const cmd = `${ffmpegPath} -y -threads 1 -i "${inp}" -vf "scale=1080:1920:force_original_aspect_ratio=decrease,pad=1080:1920:(ow-iw)/2:(oh-ih)/2,subtitles='${safeSub}':force_style='${style}'" -c:v libx264 -preset ultrafast -crf 23 -x264-params threads=1 -profile:v high -level 4.1 -c:a aac -b:a 128k -max_muxing_queue_size 256 -movflags +faststart "${out}"`;
     await execAsync(cmd, { timeout: 900000 });
     console.log(`▶ [${videoName}] Uploading to Backblaze...`);
     const auth       = await b2Auth();
